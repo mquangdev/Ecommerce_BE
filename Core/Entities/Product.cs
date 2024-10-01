632f481258d0d2e69f0b215ffeef8098bf3782fe
quangtm46 <quangtm46@fpt.com>
2024-10-01 10:39:13 +0700

    Add project files.

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class Product: BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        [Required]
+        public string PictureUrl { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
